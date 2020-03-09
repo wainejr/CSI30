121d00416f258d4f38ee2b6f32dd82fcb00c15d2
Ian Douglas Almeida Queros <ianqueros.1996@alunos.utfpr.edu.br>
2020-03-09 12:36:36 +0000

    Criando função de checar se caminho é solução

--- t01/utils.py
+++ t01/utils.py
@@ -1,9 +1,9 @@
-
 def check_if_path_is_valid_solution(path, number_of_nodes):
     """Receives a path as a list and returns a boolean indicating whether this path is a solution
     Implemented Ian
     """
-    pass
+    max_permitted_sum = (n * (n + 1))/2
+    return len(path) > 1  and path[0] == path[-1] and sum(path[:(len(path) - 1)]) == max_permitted_sum
 
 def calculate_path_cost(path, distance_matrix):
     """Calculates the cost of a path as a list based on the distance matrix and return
